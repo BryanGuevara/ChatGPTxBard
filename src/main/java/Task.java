9ddf8befc4b005c5bc6a69e513cfe9be87b55c85
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2024-06-13 12:04:39 -0600

    Create Task class with attributes and methods

---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+// src/main/java/Task.java
+public class Task {
+    private int id;
+    private String description;
+    private boolean completed;
+
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markAsCompleted() {
+        this.completed = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{id=" + id + ", description='" + description + '\'' + ", completed=" + completed + '}';
+    }
+}
