f5bb567884762b9888191aa199bfd867f0218b42
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2024-06-13 12:28:42 -0600

    Integrate TaskManager into TaskManagerApp to manage task interactions -ChatGPT

--- src/main/java/TaskManagerApp.java
+++ src/main/java/TaskManagerApp.java
@@ -2,27 +2,7 @@ package main.java;
 
 public class TaskManagerApp {
     public static void main(String[] args) {
-        try {
-            TaskList taskList = new TaskList();
-            Task task1 = new Task(1, "Write code for Task class");
-            Task task2 = new Task(2, "Write code for TaskList class");
-
-            taskList.addTask(task1);
-            taskList.addTask(task2);
-
-            System.out.println("Current tasks:");
-            for (Task task : taskList.getTasks()) {
-                System.out.println(task);
-            }
-
-            task1.markAsCompleted();
-
-            System.out.println("\nTasks after completing one:");
-            for (Task task : taskList.getTasks()) {
-                System.out.println(task);
-            }
-        } catch (Exception e) {
-            System.out.println("An error occurred: " + e.getMessage());
-        }
+        TaskManager taskManager = new TaskManager();
+        taskManager.start();
     }
 }
